9225618f3b51ccc29b87b1bc99dbb41a04318e82
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2018-11-06 16:44:08 +1100

    Initial script for vi's data

---
+++ single-dir.py
@@ -0,0 +1,14 @@
+import os
+import re
+from microscopium import preprocess as pre
+
+ROOT = '/scratch/su62/petermac/data'
+OUT = os.path.join(ROOT, 'out')
+
+os.makedirs(OUT)
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
