10cc3c1c35b9d757f2a4805af8dc57db6055d205
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2018-11-09 17:32:11 +1100

    Add robustness to convert tifs in case of bad file

--- convert-tifs.py
+++ convert-tifs.py
@@ -7,6 +7,11 @@ d = os.path.join('/scratch/su62/petermac/data', sys.argv[1])
 os.chdir(d)
 files = glob.glob('*.TIF')
 for f in files:
-    image = io.imread(f)
+    try:
+        image = io.imread(f)
+    except ValueError:
+        print(f'error reading file {f}')
+        os.rename(f, f + '.bad')
+        continue
     if image.shape == (512, 512, 3):
         io.imsave(f, image[..., 0], compress=1)
